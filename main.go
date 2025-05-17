80ddba73cc76a7c24691ce8109f64b84dfcc6b8b
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-17 18:37:29 -0400

    refactor: add logging
    Improvements were taken from the instructor's solution.

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	const port = 8080
+
 	// create an HTTP request multiplexer
 	reqMux := http.NewServeMux()
 	// set server parameters
-	server := &http.Server{Addr: ":8080", Handler: reqMux}
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: reqMux,
+	}
+
+	log.Printf("server active on port %v\n", port)
 	// start the server
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
