dbfa8f97b8df354edac76c4fd832759e817bda39
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-18 20:38:42 -0400

    feat: serve files in `./public` directory

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 )
 
 func main() {
-	const port = 8080
-
 	// create an HTTP request multiplexer
 	reqMux := http.NewServeMux()
 	// set server parameters
+	const port = 8080
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%v", port),
 		Handler: reqMux,
 	}
+
+	// map the public web root of the server to `./public`
+	webRoot := http.FileServer(http.Dir("./public"))
+	reqMux.Handle("/", webRoot)
 
 	log.Printf("server active on port %v\n", port)
 	// start the server
