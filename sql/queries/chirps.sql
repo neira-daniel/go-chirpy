b580d643a0f7400eefd2e05a02a72560e16d458f
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-26 13:50:09 -0400

    feat(db): add SQL logic to get chirps

---
+++ sql/queries/chirps.sql
@@ -0,0 +1,15 @@
+-- name: SaveChirp :one
+INSERT INTO chirps (id, created_at, updated_at, body, user_id)
+VALUES (
+    gen_random_uuid(),
+    now() AT TIME ZONE 'UTC',
+    now() AT TIME ZONE 'UTC',
+    $1,
+    $2
+)
+RETURNING *;
+
+-- name: GetChirps :many
+SELECT *
+FROM chirps
+ORDER BY created_at ASC;
