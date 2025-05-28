4700c9ef239c47fe9ca13c3cd7f0e3732262d689
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-28 00:00:44 -0400

    feat(db): add SQL logic to store hashed passwods

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,12 +1,18 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
     gen_random_uuid(),
     now() AT TIME ZONE 'UTC',
     now() AT TIME ZONE 'UTC',
-    $1
+    $1,
+    $2
 )
 RETURNING *;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1;
+
 
 -- name: ResetDatabase :exec
 DELETE FROM users;
